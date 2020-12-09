4fa589a6a1216f937a56c10127712aa2004301cb
José Alexander Bermudez <jabermudez99@misena.edu.co>
2020-12-09 17:19:04 -0500

    Se realiza actualización de paginas con las rutas ya establecidas.

--- html2/app.py
+++ html2/app.py
@@ -7,8 +7,8 @@ from markupsafe import escape
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def Index():
-    return render_template('login.html')
+def Index():    
+        return render_template('login.html')
 
 @app.route('/password', methods=['GET', 'POST'])
 def add_password():
@@ -22,6 +22,10 @@ def reg_producto():
 def reg_usuario():
     return render_template('registrousuario.html')
 
+@app.route('/actusuario', methods=["GET", "POST"])
+def act_usuario():
+    return render_template('registrousuario.html')
+
 @app.route('/galeria')
 def mostrar_galeria():
     return render_template('galeria.html')
@@ -29,6 +33,15 @@ def mostrar_galeria():
 @app.route('/accesorios')
 def mostrar_accesorio():
     return render_template('accesorio.html')
+
+@app.route('/admin', methods=['GET', 'POST'])
+def mostrar_admin():
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        contraseña = request.form['contraseña']
+        return render_template('Administrador.html')
+    else:
+        return render_template('login.html')    
 
 if __name__ == "__main__":   
    app.run(port=5000, debug=True)
